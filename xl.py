f5d1532542bb09d91acafd2669d0d1413da738a4
breath024 <79564529+breath024@users.noreply.github.com>
2026-02-27 05:26:02 +0900

    날짜포맷완성
    -

--- xl.py
+++ xl.py
@@ -3,42 +3,66 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Font, Alignment, Border, Side
 from datetime import datetime, timedelta
 
-# 엑셀 파일 경로
-path = "C:\\Users\\USER\\Desktop\\지출내역서\\입출금내역(봇용).xlsx"
-# 엑셀 파일 열기
 
-wl = load_workbook(path)
-wb = Workbook 
-dt = int(datetime.now().strftime('%m')) +1
-dlt = datetime.now() + timedelta(days=1)
-print("END")
-    # 다음날이 1일인 경우 A2 셀부터 다음달 1일까지 날짜 입력
-dlt += timedelta(days=4)
-i = 2
-print(dlt)
-while dlt.strftime('%d') != '01':
-    i += 1
-    dlt += timedelta(days=1)
-    print(dlt)
 
-def create_format(file_name):
-    # 엑셀 파일 열기
-    wl = load_workbook(file_name)
-    ws = wl.active 
-    # 다음날 날짜 계산
+dlt = datetime(datetime.now().year, 1, 1)
+print(dlt) 
+try:
+    load_workbook("입출금내_" + datetime.now().strftime('%Y')+'.xlsx')
+    print("파일이 이미 존재합니다.")
+except FileNotFoundError:
+    wb = Workbook()
+    ws = wb.active
+    
+    for i in range(0, 12):
+        wb.create_sheet(f"{i+1}월")
+        headers = ["날짜", "입금액", "출금액", "출금처","목적분류", "잔액"]
+        ws = wb[f"{i+1}월"]
+        ws.append(headers)
+        ws.column_dimensions['A'].width = 15
+        ws.column_dimensions['B'].width = 12
+        ws.column_dimensions['C'].width = 12
+        ws.column_dimensions['D'].width = 25  # 출금처는 길게
+        ws.column_dimensions['E'].width = 15
+        for col in range(1, 6):
+            ws.cell(1, col).font = Font(bold=True)
+        ws['A2'] = dlt.strftime('%Y-%m-%d')
+        dlt += timedelta(days=1)
+        i = 2
+        while dlt.strftime('%d') != '01':
+            i += 1
+            ws[f"A{i}"] = dlt.strftime('%Y-%m-%d')
+            dlt += timedelta(days=1)
+    wb.save("입출금내역_" + datetime.now().strftime('%Y')+'.xlsx')
+
+
+
+
+def create_format():
+
     dlt = datetime.now() + timedelta(days=1)
-    # 다음날이 1일인 경우 A2 셀부터 다음달 1일까지 날짜 입력
-    if dlt.strftime('%d') == '01':
-        dlt += 1
+    try:
+        load_workbook("입출금내역_" + datetime.now().strftime('%Y')+'.xlsx')
+        print("파일이 이미 존재합니다.")
+        return 0
+    except FileNotFoundError:
+        wb = Workbook()
+        ws = wb.active
+
+        dlt += timedelta(days=1)
+        headers = ["날짜", "입금액", "출금액", "출금처", "잔액"]
+        ws.append(headers)
+        ws.column_dimensions['A'].width = 15
+        ws.column_dimensions['B'].width = 12
+        ws.column_dimensions['C'].width = 12
+        ws.column_dimensions['D'].width = 25  # 출금처는 길게
+        ws.column_dimensions['E'].width = 15
+        for col in range(1, 6):
+            ws.cell(1, col).font = Font(bold=True)
         ws['A2'] = datetime.now().strftime('%Y-%m-%d')
         i = 2
         while dlt.strftime('%d') != '01':
             i += 1
             ws[f"A{i}"] = dlt.strftime('%Y-%m-%d')
             dlt += timedelta(days=1)
-
-
-    # A1 셀에 현재 날짜와 시간 입력
-    ws['A1'] = dt
-    # 엑셀 파일 저장
-    wl.save(file_name)
+        wb.save("입출금내역_" + datetime.now().strftime('%Y')+'.xlsx')
